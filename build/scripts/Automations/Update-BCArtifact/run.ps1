b81cfdadd7e91be0601395b4883c56b3a99424d5
mazhelez <mazhelez@microsoft.com>
2024-03-13 12:16:14 +0100

    WIP

--- build/scripts/Automations/Update-BCArtifact/run.ps1
+++ build/scripts/Automations/Update-BCArtifact/run.ps1
@@ -3,27 +3,20 @@
     Updates the BCArtifact version in the AL-Go settings file (artifact property)
 .DESCRIPTION
     This script will update the BCArtifact version in the AL-Go settings file (artifact property) to the latest version available on the BC artifacts feed (bcinsider/bcartifacts storage account).
-    If the version is updated, a new branch will be created and a pull request will be created to merge the changes into the target branch.
 .PARAMETER TargetBranch
-    The branch to create the pull request to
-.PARAMETER Actor
-    The name of the user that will be used as commit author
+    Unused. Always passed by the automation handler.
 #>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'TargetBranch', Justification = 'Always passed by the automations handler')]
 param(
-    [Parameter(Mandatory = $true)]
-    [string]$Repository,
-    [Parameter(Mandatory = $true)]
-    [string]$TargetBranch,
-    [Parameter(Mandatory = $true)]
-    [string]$Actor
+    [Parameter(Mandatory = $false)]
+    [string]$TargetBranch
 )
 
 # BC Container Helper is needed to fetch the latest artifact version
 Install-Module -Name BcContainerHelper -AllowPrerelease -Force
 Import-Module BcContainerHelper
 
-Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
-Import-Module $PSScriptRoot\AutomatedSubmission.psm1
+Import-Module $PSScriptRoot\..\..\EnlistmentHelperFunctions.psm1
 
 function UpdateBCArtifactVersion() {
     $artifactValue = Get-ConfigValue -Key "artifact" -ConfigType AL-Go
@@ -46,22 +39,17 @@ function UpdateBCArtifactVersion() {
         $artifactValue = $artifactValue -replace $currentArtifactVersion, $latestArtifactVersion
         Set-ConfigValue -Key "artifact" -Value $artifactValue -ConfigType AL-Go
 
-        return $true
+        return $latestArtifactVersion
     }
 
-    return $false
+    return $null
 }
 
-$pullRequestTitle = "[$TargetBranch] Update BC Artifact version"
-$BranchName = New-TopicBranchIfNeeded -Repository $Repository -Category "UpdateBCArtifactVersion/$TargetBranch" -PullRequestTitle $pullRequestTitle
+$newVersion = UpdateBCArtifactVersion
 
-$updatesAvailable = UpdateBCArtifactVersion
-
-if ($updatesAvailable) {
-    # Create branch and push changes
-    Set-GitConfig -Actor $Actor
-    Push-GitBranch -BranchName $BranchName -Files @(".github/AL-Go-Settings.json") -CommitMessage $pullRequestTitle
-    New-GitHubPullRequest -Repository $Repository -BranchName $BranchName -TargetBranch $TargetBranch -label "automation" -PullRequestDescription "Fixes AB#420000"
-} else {
-    Write-Host "No updates available"
+if ($newVersion) {
+    return @{
+        'Files' = @(".github/AL-Go-Settings.json")
+        'Message' = "Update BCArtifact version in AL-Go settings file. New version: $newVersion"
+    }
 }
--- build/scripts/Automations/run.ps1
+++ build/scripts/Automations/run.ps1
@@ -1,24 +1,83 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string] $PackageFilter = '*'
+    [string[]] $Include,
+    [Parameter(Mandatory=$true)]
+    [string] $Repository,
+    [Parameter(Mandatory=$true)]
+    [string] $TargetBranch,
+    [Parameter(Mandatory=$true)]
+    [string] $Actor
 )
 
-Import-Module "$PSScriptRoot\..\EnlistmentHelperFunctions.psm1" -DisableNameChecking
+function OpenPR($AvailableUpdates, $Repository, $TargetBranch, $Actor) {
+    Import-Module $PSScriptRoot\AutomatedSubmission.psm1
 
-$baseFolder = Get-BaseFolder
 
-$packagesFolder = Join-Path $baseFolder "build/scripts/Automations"
-$packagePaths = Get-ChildItem -Path $packagesFolder -Directory -Filter $PackageFilter
-
-$availableUpdates = @()
-foreach ($packagePath in $packagePaths) {
-    $packageUpdates = . "$packagePath\UpdatePackage.ps1"
-
-    if ($packageUpdates) {
-        $availableUpdates += $packageUpdates
+    Write-Host "Opening PR for the following updates:"
+    $AvailableUpdates | ForEach-Object {
+        Write-Host "  - $($_.Name): $($_.Result)"
     }
+
+    Set-GitConfig -Actor $Actor
+    $branch = New-TopicBranch -Category 'updates'
+
+    # Open PR with a commit for each update
+    $AvailableUpdates | ForEach-Object {
+        $automationName = $_.Name
+        $automationResult = $_.Result
+
+        $commitMessage = "$($automationResult.Message)"
+        $commitFiles = $automationResult.Files
+
+        git add $commitFiles
+        git commit -m $commitMessage
+    }
+
+    git push -u origin $branch
+
+    New-GitHubPullRequest -Repository $Repository -BranchName $branch -TargetBranch $TargetBranch -Title "Automated updates" -Body "Automated updates"
+
 }
 
+$automationsFolder = $PSScriptRoot
+$automationsPaths = Get-ChildItem -Path $automationsFolder -Directory -Filter $Filter
 
+# Filter out the automations that are not included
+if($Include) {
+    $automationsPaths = $automationsPaths | Where-Object { $Include -contains $_.Name }
+}
 
+$availableUpdates = @()
+$failedAutomations = @()
 
+foreach ($automationPath in $automationsPaths) {
+    $automationName = $automationPath.Name
+    Write-Host "Running automation $automationName"
+
+    try {
+        $automationResult = . (Join-Path $automationPath.FullName 'run.ps1') -TargetBranch $TargetBranch
+    } catch {
+        Write-Host "Error running automation: $($_.Exception.Message)" -ForegroundColor Red
+        $failedAutomations += @($automationName)
+    }
+
+    if ($automationResult) {
+        $availableUpdates += @{
+            'Name' = $automationName
+            'Result' = $automationResult
+        }
+    }
+
+    Write-Host "Automation $automationName completed"
+}
+
+if($availableUpdates.Count -eq 0) {
+    Write-Host "No available updates"
+    return
+}
+
+OpenPR -AvailableUpdates $availableUpdates -Repository $Repository -TargetBranch $TargetBranch -Actor $Actor
+
+if ($failedAutomations) {
+    throw "The following automantions failed: $($failedAutomations -join ', '). See logs above for details"
+}
