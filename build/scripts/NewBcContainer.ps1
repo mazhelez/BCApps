bc52bd28db898b1f0aa1e8c9fb3da1e371541c90
Maria Zhelezova <55603517+m-zhelezova@users.noreply.github.com>
2024-06-26 15:12:16 +0200

    Update NewBcContainer.ps1

--- build/scripts/NewBcContainer.ps1
+++ build/scripts/NewBcContainer.ps1
@@ -14,7 +14,7 @@ New-BcContainer @parameters
 
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
-    Write-Host "Removing $($_.Name). Lorem ipsum part 2 (from fork. Changed!!!)
+    Write-Host "Removing $($_.Name). Lorem ipsum part 2 (from fork. Changed!!!)"
     Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
 }
 
