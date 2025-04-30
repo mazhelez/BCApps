b14cc23cd9d3c800347b994076fb75d27f230b65
mazhelez <mazhelez@users.noreply.github.com>
2025-04-30 09:01:43 +0000

    [releases/24.0@6cd1000] Update AL-Go System Files from mazhelez/AL-Go@add-settings-schema - 9288b07

--- build/projects/Performance Toolkit/.AL-Go/cloudDevEnv.ps1
+++ build/projects/Performance Toolkit/.AL-Go/cloudDevEnv.ps1
@@ -42,9 +42,9 @@ Write-Host -ForegroundColor Yellow @'
 
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
-$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/mazhelez/AL-Go/update-al-go-multiple-branches/Actions/Github-Helper.psm1' -folder $tmpFolder
-$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/mazhelez/AL-Go/update-al-go-multiple-branches/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
-DownloadHelperFile -url 'https://raw.githubusercontent.com/mazhelez/AL-Go/update-al-go-multiple-branches/Actions/Packages.json' -folder $tmpFolder | Out-Null
+$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/mazhelez/AL-Go/add-settings-schema/Actions/Github-Helper.psm1' -folder $tmpFolder
+$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/mazhelez/AL-Go/add-settings-schema/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
+DownloadHelperFile -url 'https://raw.githubusercontent.com/mazhelez/AL-Go/add-settings-schema/Actions/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
